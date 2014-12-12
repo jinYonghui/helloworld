b846bf24bdc833b96160eff20b663f0bf6766074
jinyonghui <jinyonghui@kingteller.com.cn>
2014-12-12 17:03:12 +0800

    create a project

---
+++ Zongjie/src/com/example/zongjie/StringUtil.java
@@ -0,0 +1,22 @@
+package com.example.zongjie;
+
+public class StringUtil {
+
+	public int stringToInt(String str){
+		int index = 0;
+		index = Integer.parseInt(str);
+		return index;
+	}
+	
+	public float stringToFloat(String str){
+		float index = 0;
+		index = Float.parseFloat(str);
+		return index;
+	}
+	
+	public double stringToDouble(String str){
+		double index = 0;
+		index = Double.parseDouble(str);
+		return index;
+	}
+}
